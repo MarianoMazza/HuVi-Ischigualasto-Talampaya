643bee8e8caf0dca4c3e6cec37203f9c9c27f86f
Mariano Mazza <mazza.mariano@hotmail.com>
2021-07-01 18:18:33 -0300

    Only two maps left to build

--- Assets/Scripts/WindEvent.cs
+++ Assets/Scripts/WindEvent.cs
@@ -6,6 +6,14 @@ public class WindEvent : MonoBehaviour
 {
     [SerializeField] AudioClip windSound;
     [SerializeField] AudioSource audioSource;
+    [SerializeField] ParticleSystem sandstorm;
+    Animator playerAnimator;
+
+    private void Start()
+    {
+        playerAnimator = this.GetComponent<Animator>();
+        sandstorm.Stop();
+    }
 
     IEnumerator RandomWindTiming(float time)
     {
@@ -14,10 +22,12 @@ public class WindEvent : MonoBehaviour
         if (Random.Range(0, 100) < 50)
         {
             this.GetComponent<MOVER>().speed = 4;
-            audioSource.Play();
+            playerAnimator.SetTrigger("WindSoundOn");
+            sandstorm.Play();
             yield return new WaitForSeconds(time);
-            audioSource.Stop();
+            playerAnimator.SetTrigger("WindSoundOff");
             this.GetComponent<MOVER>().speed = 9;
+            sandstorm.Stop();
         }
 
         StartCoroutine(RandomWindTiming(time));
@@ -29,7 +39,8 @@ public class WindEvent : MonoBehaviour
     {
         if (collision.gameObject.CompareTag("WindStart"))
         {
-            StartCoroutine(RandomWindTiming(5));
+            StartCoroutine(RandomWindTiming(10));
+            audioSource.Play();
         }
         else if(collision.gameObject.CompareTag("Protection"))
         {
