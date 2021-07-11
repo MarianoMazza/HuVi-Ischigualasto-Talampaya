f65b296f941bec22e9cfc32d92cf4722a88467c7
Mariano Mazza <mazza.mariano@hotmail.com>
2021-07-11 12:30:07 -0300

    Only one map left

--- Assets/Scripts/DinosaurEvent.cs
+++ Assets/Scripts/DinosaurEvent.cs
@@ -4,23 +4,32 @@ using UnityEngine;
 
 public class DinosaurEvent : MonoBehaviour
 {
-    [SerializeField] GameObject dinosaur;
-
-    void Start()
-    {
-        
-    }
-
-    void Update()
-    {
-        
-    }
+    [SerializeField] GameObject leg;
+    [SerializeField] GameObject arm;
+    [SerializeField] GameObject skull;
+    [SerializeField] GameObject player;
+    [SerializeField] AudioSource audioSource;
 
     private void OnTriggerEnter(Collider collision)
     {
         if (collision.gameObject.CompareTag("DinosaurStart"))
         {
-            dinosaur.GetComponent<Animator>().enabled = true;
+            leg.GetComponent<Animator>().enabled = true;
+            skull.GetComponent<Animator>().enabled = true;
+            arm.GetComponent<Animator>().enabled = true;
+            audioSource.Play();
+            player.GetComponent<Animator>().SetTrigger("WindSoundOn");
+            StartCoroutine(WaitThisTime(5));
+
         }
     }
+
+    IEnumerator WaitThisTime(float time)
+    {
+        yield return new WaitForSeconds(time);
+        player.GetComponent<Animator>().SetTrigger("WindSoundOff");
+        yield return new WaitForSeconds(time);
+        audioSource.Stop();
+    }
+
 }
