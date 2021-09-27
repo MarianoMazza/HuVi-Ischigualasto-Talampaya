951aa344eda26c73682386b708b2d693841f9242
Mariano Mazza <mazza.mariano@hotmail.com>
2021-09-27 19:32:09 -0300

    merging

---
+++ Assets/CollectiblesSign.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollectiblesSign : MonoBehaviour
+{
+    GameManager gameManager;
+    public string zoneName;
+    [SerializeField] 
+    GameObject[] collectibles = new GameObject[5];
+
+    private void Awake()
+    {
+        gameManager = FindObjectOfType<GameManager>();
+        gameManager.ReceiveCollectiblesSign(this);
+    }
+
+    public void ReceiveFoundCollectables(bool[] foundCollectibles)
+    {
+        for (int i = 0; i < foundCollectibles.Length; i++)
+        {
+            if (foundCollectibles[i])
+            {
+                collectibles[i].SetActive(true);
+            }
+        }
+    }
+}
